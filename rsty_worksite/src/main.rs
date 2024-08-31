340fd5b964a568421babfba5d34b10cd7544798b
RyanWri <ranman37@gmail.com>
2024-08-31 23:31:25 +0300

    hello world leptos

--- rsty_worksite/src/main.rs
+++ rsty_worksite/src/main.rs
@@ -1,8 +1,6 @@
 use leptos::*;
-use app::App;
-
-mod app;
 
 fn main() {
-    mount_to_body(|| view! { <App /> });
+    mount_to_body(|| view! { <p>"Hello, world!"</p> });
+    console_error_panic_hook::set_once();
 }
