d1b6952304ea3eb7d790ab85e7902106f05c3084
RyanWri <ranman37@gmail.com>
2024-09-02 19:37:21 +0300

    button component without scope

--- rsty_worksite/src/main.rs
+++ rsty_worksite/src/main.rs
@@ -1,6 +1,14 @@
 use leptos::*;
+mod components;  // Include the components module
+
+#[component]
+fn App() -> impl IntoView {
+    view! {
+        <components::button::Button title="Click Me".to_string() />
+    }
+}
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> });
+    mount_to_body(|| view! { <App /> });
     console_error_panic_hook::set_once();
 }
